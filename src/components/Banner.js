72e50dbe84f693fa63cd280d10ec4476320ff449
siwon99 <kimsiwon0707@gmail.com>
2023-05-28 17:19:55 +0900

    Image Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,7 @@
 import axios from '../api/axios';
 import React, { useEffect, useState } from 'react';
 import requests from '../api/request';
+import "./Banner.css";
 
 const Banner = () => {
 
@@ -19,16 +20,43 @@ const Banner = () => {
     ].id
 
     //특정 영화의 더 상세한 정보 가져오기(비디오 정보도 포함)
-    const {data: movieDetail} = await axios.get(`movie/${movieId}`, {
-      params: {append_to_response: "videos"}
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response: "videos" }
     })
     setMovie(movieDetail);
   }
 
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substring(0, n) + '...' : str;
+  }
+
   return (
-    <div>
-      
-    </div>
+    <header
+      className='banner'
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover"
+      }}
+    >
+      <div style={{ fontSize: 10 }} className='banner__contents'>
+        <h1 className='banner__title'>
+          {movie.title || movie.name || movie.original_name}
+        </h1>
+
+        <div className='banner__buttons'>
+          {movie?.videos?.results[0]?.key &&
+            <button className='banner__button play'>
+              Play
+            </button>
+          }
+        </div>
+        <p className='banner__description'>
+          {truncate(movie.overview, 100)}
+        </p>
+      </div>
+        <div className='banner--fadeBottom' />
+    </header>
   )
 }
 
