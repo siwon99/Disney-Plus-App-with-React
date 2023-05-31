8553a0d443116f502f2374e001a3c1670b2ff724
siwon99 <kimsiwon0707@gmail.com>
2023-05-31 22:57:21 +0900

    Modal CSS

--- src/components/Row.js
+++ src/components/Row.js
@@ -56,7 +56,7 @@ const Row = ({title, id, fetchUrl}) => {
         </div>
       </div>
 
-      {setModalOpen && 
+      {modalOpen && 
       <MovieModal 
       {...movieSelected}
       setModalOpen = {setModalOpen} />
