a47347e6015720c5b2c4e42122517d9d0efc1c93
siwon99 <kimsiwon0707@gmail.com>
2023-06-03 15:46:14 +0900

    Search Poster

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Outlet, Route, Routes } from 'react-router-dom';
+import styled from 'styled-components';
+import requests from './api/request';
 import './App.css';
+import Banner from './components/Banner';
+import Category from './components/Category';
 import Nav from './components/Nav';
+import Row from './components/Row';
 import LoginPage from './pages/LoginPage';
 import MainPage from './pages/MainPage';
 import SearchPage from './pages/SearchPage';
 import DetailPage from './pages/DetailPage';
 
-const Layout = () => {
-  return (
+const Layout =() => {
+  return(
     <div>
       <Nav />
 
@@ -18,17 +23,16 @@ const Layout = () => {
 
 function App() {
   return (
-    <div className='app'>
+   <div className='app'>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<LoginPage />} />
           <Route path="main" element={<MainPage />} />
           <Route path=":movieId" element={<DetailPage />} />
-          <Route path=":search" element={<SearchPage />} />
+          <Route path="search" element={<SearchPage />} />
         </Route>
       </Routes>
-
-    </div>
+   </div>
   );
 }
 
